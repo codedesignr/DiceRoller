fb94c35344b88546af4f46d75a7a606eb52656e4
Adam Lopez <adamll83@gmail.com>
2019-05-18 17:56:05 -0400

    add second die

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -8,21 +8,28 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var diceImage1 : ImageView
+    private lateinit var diceImage2 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-
-//        val countButton: Button = findViewById(R.id.add_button)
-//        countButton.setOnClickListener { incrementCount() }
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
-        val diceImage: ImageView = findViewById(R.id.dice_image)
-        val drawableResource = when (randomInt) {
+        diceImage1.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage() : Int {
+        val randomInt =  Random().nextInt(6) + 1
+        return when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -30,21 +37,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
-
     }
 
-//    private fun incrementCount() {
-//        val diceImage: ImageView = findViewById(R.id.dice_image)
-//        if(diceImage.image == R.drawable.dice_1) {
-//            resultText.text = "1"
-//        } else {
-//            var resultInt = resultText.text.toString().toInt()
-//
-//            if (resultInt < 6) {
-//                resultInt++
-//                resultText.text = resultInt.toString()
-//            }
-//        }
-//    }
 }
